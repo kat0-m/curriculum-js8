1a3be918b22d19e058b76f78612af29f2124e240
k-masako <kato_masako@outlook.jp>
2024-03-26 01:47:08 +0900

    イベント発火 基礎編

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+window.addEventListener('load', function(){
+
+    const pullDownButton = document.getElementById("lists")
+    console.log("load")
+  
+    pullDownButton.addEventListener('mouseover', function(){
+      console.log("mouseover")
+    })
+
+    pullDownButton.addEventListener('mouseout', function(){
+        console.log("mouseout")
+      })
+})
+
