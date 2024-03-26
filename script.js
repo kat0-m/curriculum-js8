d1ebb3d928dfa7930de63964a61638ed2aee6a2f
k-masako <kato_masako@outlook.jp>
2024-03-26 17:39:20 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 window.addEventListener('load', function(){
 
     const pullDownButton = document.getElementById("lists")
-    console.log("load")
+    const pullDownParents = document.getElementById("pull-down")
   
     pullDownButton.addEventListener('mouseover', function(){
-      pullDownButton.setAttribute("style","background-color:rgb(60, 126, 240);")
+      this.setAttribute("style","background-color:rgb(60, 126, 240);")
       console.log("mouseover,blue")
     })
 
     pullDownButton.addEventListener('mouseout', function(){
-      pullDownButton.removeAttribute("style","background-c0lor:rgb(243, 96, 96);")
+      this.removeAttribute("style","background-c0lor:rgb(243, 96, 96);")
         console.log("mouseout,red")
     })
 
     pullDownButton.addEventListener('click', function(){
-      pullDownButton.setAttribute("style","background-color:rgb(51, 184, 91);")
-        console.log("click,green")
-    })
+      if(pullDownParents.getAttribute('style') == "display:block;"){
+        pullDownParents.removeAttribute("style","display:block;")
+        console.log("非表示")
+      }else{
+        pullDownParents.setAttribute("style","display:block;")
+        console.log("表示")
+      }
+      })
 })
+
 
